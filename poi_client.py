49c244e0a8231b4c98038eaa9044987f4288991c
chenzhen <chenzhen@u51.com>
2018-11-29 14:54:24 +0800

    httpserver

---
+++ poi_client.py
@@ -0,0 +1,386 @@
+# coding=utf-8
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+import grpc
+import tensorflow as tf
+import tokenization
+import collections
+
+from run_poi import SquadExample, _check_is_max_context, InputFeatures, RawResult, _get_best_indexes, get_final_text, \
+    _compute_softmax
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+channel = grpc.insecure_channel("localhost:8500")
+stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+tokenizer = tokenization.FullTokenizer(vocab_file='chinese_model/vocab.txt')
+
+
+def read_poi_examples(str):
+    datas = [{'context': str}]
+
+    def is_whitespace(c):
+        if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+            return True
+        return False
+
+    examples = []
+    id = 0
+    for paragraph in datas:
+        paragraph_text = paragraph["context"]
+        doc_tokens = []
+        char_to_word_offset = []
+        prev_is_whitespace = True
+        for c in paragraph_text:
+            if is_whitespace(c):
+                prev_is_whitespace = True
+            else:
+                if prev_is_whitespace:
+                    doc_tokens.append(c)
+                else:
+                    doc_tokens[-1] += c
+                prev_is_whitespace = False
+            char_to_word_offset.append(len(doc_tokens) - 1)
+
+        qas_id = id
+        id = id + 1
+        start_position = None
+        end_position = None
+        orig_answer_text = None
+        is_impossible = False
+
+        example = SquadExample(
+            qas_id=qas_id,
+            doc_tokens=doc_tokens,
+            orig_answer_text=orig_answer_text,
+            start_position=start_position,
+            end_position=end_position,
+            is_impossible=is_impossible)
+        examples.append(example)
+
+    return examples
+
+
+def convert_examples_to_features(examples, tokenizer, max_seq_length,
+                                 doc_stride):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    unique_id = 1000000000
+    result = []
+    for (example_index, example) in enumerate(examples):
+
+        tok_to_orig_index = []
+        orig_to_tok_index = []
+        all_doc_tokens = []
+        for (i, token) in enumerate(example.doc_tokens):
+            orig_to_tok_index.append(len(all_doc_tokens))
+            sub_tokens = tokenizer.tokenize(token)
+            for sub_token in sub_tokens:
+                tok_to_orig_index.append(i)
+                all_doc_tokens.append(sub_token)
+
+        tok_start_position = None
+        tok_end_position = None
+
+        # The -2 accounts for [CLS], [SEP]
+        max_tokens_for_doc = max_seq_length - 2
+
+        # We can have documents that are longer than the maximum sequence length.
+        # To deal with this we do a sliding window approach, where we take chunks
+        # of the up to our max length with a stride of `doc_stride`.
+        _DocSpan = collections.namedtuple(  # pylint: disable=invalid-name
+            "DocSpan", ["start", "length"])
+        doc_spans = []
+        start_offset = 0
+        while start_offset < len(all_doc_tokens):
+            length = len(all_doc_tokens) - start_offset
+            if length > max_tokens_for_doc:
+                length = max_tokens_for_doc
+            doc_spans.append(_DocSpan(start=start_offset, length=length))
+            if start_offset + length == len(all_doc_tokens):
+                break
+            start_offset += min(length, doc_stride)
+
+        for (doc_span_index, doc_span) in enumerate(doc_spans):
+            tokens = []
+            token_to_orig_map = {}
+            token_is_max_context = {}
+            segment_ids = []
+            tokens.append("[CLS]")
+            segment_ids.append(0)
+
+            for i in range(doc_span.length):
+                split_token_index = doc_span.start + i
+                token_to_orig_map[len(tokens)] = tok_to_orig_index[split_token_index]
+
+                is_max_context = _check_is_max_context(doc_spans, doc_span_index,
+                                                       split_token_index)
+                token_is_max_context[len(tokens)] = is_max_context
+                tokens.append(all_doc_tokens[split_token_index])
+                segment_ids.append(0)
+            tokens.append("[SEP]")
+            segment_ids.append(0)
+
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            input_mask = [1] * len(input_ids)
+
+            # Zero-pad up to the sequence length.
+            while len(input_ids) < max_seq_length:
+                input_ids.append(0)
+                input_mask.append(0)
+                segment_ids.append(0)
+
+            assert len(input_ids) == max_seq_length
+            assert len(input_mask) == max_seq_length
+            assert len(segment_ids) == max_seq_length
+
+            start_position = None
+            end_position = None
+
+            feature = InputFeatures(
+                unique_id=unique_id,
+                example_index=example_index,
+                doc_span_index=doc_span_index,
+                tokens=tokens,
+                token_to_orig_map=token_to_orig_map,
+                token_is_max_context=token_is_max_context,
+                input_ids=input_ids,
+                input_mask=input_mask,
+                segment_ids=segment_ids,
+                start_position=start_position,
+                end_position=end_position,
+                is_impossible=example.is_impossible)
+
+            # Run callback
+            result.append(feature)
+    return result
+
+
+def remote_call(evl_features):
+    eval_feature = evl_features[0]
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'poi'
+    request.model_spec.signature_name = 'serving_default'
+
+    def create_int_feature(values):
+        feature = tf.train.Feature(
+            int64_list=tf.train.Int64List(value=list(values)))
+        return feature
+
+    features = collections.OrderedDict()
+    features["unique_ids"] = create_int_feature([eval_feature.unique_id])
+    features["input_ids"] = create_int_feature(eval_feature.input_ids)
+    features["input_mask"] = create_int_feature(eval_feature.input_mask)
+    features["segment_ids"] = create_int_feature(eval_feature.segment_ids)
+    example = tf.train.Example(features=tf.train.Features(feature=features)).SerializeToString()
+    request.inputs['examples'].CopyFrom(tf.make_tensor_proto([example], dtype=tf.string))
+    response = stub.Predict(request, 60.0)
+
+    end_logits = [float(x) for x in response.outputs['end_logits'].float_val]
+    start_logits = [float(x) for x in response.outputs['start_logits'].float_val]
+    unique_id = int(response.outputs['unique_ids'].int64_val[0])
+    all_results = [RawResult(unique_id=unique_id, start_logits=start_logits, end_logits=end_logits)]
+    return all_results
+
+
+def write_predictions(all_examples, all_features, all_results, n_best_size,
+                      max_answer_length):
+    example_index_to_features = collections.defaultdict(list)
+    for feature in all_features:
+        example_index_to_features[feature.example_index].append(feature)
+
+    unique_id_to_result = {}
+    for result in all_results:
+        unique_id_to_result[result.unique_id] = result
+
+    _PrelimPrediction = collections.namedtuple(  # pylint: disable=invalid-name
+        "PrelimPrediction",
+        ["feature_index", "start_index", "end_index", "start_logit", "end_logit"])
+
+    all_predictions = []
+
+    for (example_index, example) in enumerate(all_examples):
+        features = example_index_to_features[example_index]
+
+        prelim_predictions = []
+        # keep track of the minimum score of null start+end of position 0
+        score_null = 1000000  # large and positive
+        min_null_feature_index = 0  # the paragraph slice with min mull score
+        null_start_logit = 0  # the start logit at the slice with min null score
+        null_end_logit = 0  # the end logit at the slice with min null score
+        for (feature_index, feature) in enumerate(features):
+            result = unique_id_to_result[feature.unique_id]
+            start_indexes = _get_best_indexes(result.start_logits, n_best_size)
+            end_indexes = _get_best_indexes(result.end_logits, n_best_size)
+            # if we could have irrelevant answers, get the min score of irrelevant
+
+            feature_null_score = result.start_logits[0] + result.end_logits[0]
+            if feature_null_score < score_null:
+                score_null = feature_null_score
+                min_null_feature_index = feature_index
+                null_start_logit = result.start_logits[0]
+                null_end_logit = result.end_logits[0]
+
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    # We could hypothetically create invalid predictions, e.g., predict
+                    # that the start of the span is in the question. We throw out all
+                    # invalid predictions.
+                    if start_index >= len(feature.tokens):
+                        continue
+                    if end_index >= len(feature.tokens):
+                        continue
+                    if start_index not in feature.token_to_orig_map:
+                        continue
+                    if end_index not in feature.token_to_orig_map:
+                        continue
+                    if not feature.token_is_max_context.get(start_index, False):
+                        continue
+                    if end_index < start_index:
+                        continue
+                    length = end_index - start_index + 1
+                    if length > max_answer_length:
+                        continue
+                    prelim_predictions.append(
+                        _PrelimPrediction(
+                            feature_index=feature_index,
+                            start_index=start_index,
+                            end_index=end_index,
+                            start_logit=result.start_logits[start_index],
+                            end_logit=result.end_logits[end_index]))
+
+        prelim_predictions.append(
+            _PrelimPrediction(
+                feature_index=min_null_feature_index,
+                start_index=0,
+                end_index=0,
+                start_logit=null_start_logit,
+                end_logit=null_end_logit))
+
+        prelim_predictions = sorted(
+            prelim_predictions,
+            key=lambda x: (x.start_logit + x.end_logit),
+            reverse=True)
+
+        _NbestPrediction = collections.namedtuple(  # pylint: disable=invalid-name
+            "NbestPrediction", ["text", "start_logit", "end_logit"])
+
+        seen_predictions = {}
+        nbest = []
+        for pred in prelim_predictions:
+            if len(nbest) >= n_best_size:
+                break
+            feature = features[pred.feature_index]
+            if pred.start_index > 0:  # this is a non-null prediction
+                tok_tokens = feature.tokens[pred.start_index:(pred.end_index + 1)]
+                orig_doc_start = feature.token_to_orig_map[pred.start_index]
+                orig_doc_end = feature.token_to_orig_map[pred.end_index]
+                orig_tokens = example.doc_tokens[orig_doc_start:(orig_doc_end + 1)]
+                tok_text = " ".join(tok_tokens)
+
+                # De-tokenize WordPieces that have been split off.
+                tok_text = tok_text.replace(" ##", "")
+                tok_text = tok_text.replace("##", "")
+
+                # Clean whitespace
+                tok_text = tok_text.strip()
+                tok_text = " ".join(tok_text.split())
+                orig_text = " ".join(orig_tokens)
+
+                final_text = get_final_text(tok_text, orig_text, True)
+                if final_text in seen_predictions:
+                    continue
+
+                seen_predictions[final_text] = True
+            else:
+                final_text = ""
+                seen_predictions[final_text] = True
+
+            nbest.append(
+                _NbestPrediction(
+                    text=final_text,
+                    start_logit=pred.start_logit,
+                    end_logit=pred.end_logit))
+
+        # if we didn't inlude the empty option in the n-best, inlcude it
+
+        if "" not in seen_predictions:
+            nbest.append(
+                _NbestPrediction(
+                    text="", start_logit=null_start_logit,
+                    end_logit=null_end_logit))
+        # In very rare edge cases we could have no valid predictions. So we
+        # just create a nonce prediction in this case to avoid failure.
+        if not nbest:
+            nbest.append(
+                _NbestPrediction(text="empty", start_logit=0.0, end_logit=0.0))
+
+        assert len(nbest) >= 1
+
+        total_scores = []
+        best_non_null_entry = None
+        for entry in nbest:
+            total_scores.append(entry.start_logit + entry.end_logit)
+            if not best_non_null_entry:
+                if entry.text:
+                    best_non_null_entry = entry
+
+        probs = _compute_softmax(total_scores)
+
+        nbest_json = []
+        for (i, entry) in enumerate(nbest):
+            output = collections.OrderedDict()
+            output["text"] = entry.text
+            output["probability"] = probs[i]
+            output["start_logit"] = entry.start_logit
+            output["end_logit"] = entry.end_logit
+            nbest_json.append(output)
+
+        assert len(nbest_json) >= 1
+
+        all_predictions.append(nbest_json)
+
+    return all_predictions
+
+
+def clean_text(txt):
+    return tokenizer.basic_tokenizer._run_strip_accents(tokenizer.basic_tokenizer._clean_text(txt))
+
+
+@app.route('/poi',methods=['POST'])
+def get_poi():
+    input_txt = request.json['input']
+    input_txt = clean_text(input_txt)
+    eval_examples = read_poi_examples(input_txt)
+    eval_features = convert_examples_to_features(
+        examples=eval_examples,
+        tokenizer=tokenizer,
+        max_seq_length=384,
+        doc_stride=128)
+    all_results = remote_call(eval_features)
+
+    eval_results = write_predictions(eval_examples, eval_features, all_results, 2, 30)[0]
+    eval_result = eval_results[0]
+    print(input_txt)
+
+    if eval_result['end_logit'] < 0 or eval_result['start_logit'] < 0:
+        print('[NONE]')
+        return jsonify({})
+    else:
+        print(eval_result)
+        return jsonify(eval_result)
+
+
+@app.route('/xxx')
+def get_xxx():
+    return jsonify({'xxx': 'xxx'})
+
+
+if __name__ == "__main__":
+    app.config['JSON_AS_ASCII'] = False
+    app.run(host='0.0.0.0')
